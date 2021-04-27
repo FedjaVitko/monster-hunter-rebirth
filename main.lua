ac449028f1d68ed2350c76a8adc46b7471bb51e1
TheoVitkovskiy <fvitkovski@mail.de>
2021-04-27 14:15:31 +0200

    weird glitchy spawn

--- main.lua
+++ main.lua
@@ -1,26 +1,33 @@
+-- constants
 local ModFunctions = {}
 local Mod = RegisterMod("Monster Hunteri Rebirth", 1);
-
-local debugText = 'default'
 
 local Enemies = {
     Mosquito = Isaac.GetEntityVariantByName("Mosquito")
 }
 
-function ModFunctions:render()
+-- state
+local debugText = 'default'
+local boneKnightSpawned = false
+
+function ModFunctions:Render()
     Isaac.RenderText(debugText, 150, 150, 255, 0, 0, 50);
 end
 
 function ModFunctions:EnemyMosquito(entityNPC)
     local npcData = entityNPC:GetData()
 
-    Isaac.Spawn(EntityType.ENTITY_STONEY, Enemies.Mosquito, 0, entityNPC.Position, Vector(0,0), entityNPC.SpawnerEntity)
+    if not boneKnightSpawned then
+        debugText = 'the enemy should now be spawned'
+        Isaac.Spawn(EntityType.ENTITY_BONE_KNIGHT, Enemies.Mosquito, 0, entityNPC.Position, Vector(0,0), entityNPC.SpawnerEntity)
+        boneKnightSpawned = true
+    end
 
     -- if (entityNPC:IsActiveEnemy()) then
     --     debugText = entityNPC.StateFrame
     -- end
 end
 
-Mod:AddCallback(ModCallbacks.MC_POST_RENDER, ModFunctions.render);
+Mod:AddCallback(ModCallbacks.MC_POST_RENDER, ModFunctions.Render);
 
 Mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, ModFunctions.EnemyMosquito); -- TODO: not sure which EntityType to pick
